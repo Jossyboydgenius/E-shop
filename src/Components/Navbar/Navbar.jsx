6e8ec71d2600576fcc367acb501f111c2def8f24
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-02 08:36:44 +0000

    E-commerce

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+      </div>
     </div>
   )
 }
