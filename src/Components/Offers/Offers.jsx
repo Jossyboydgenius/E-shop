8a530039b9a4153b5fdae0bd41a7bebfaf1485c7
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-02 19:36:38 +0000

    E-commerce

---
+++ src/Components/Offers/Offers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Offers.css'
+
+const Offers = () => {
+  return (
+    <div className='offers'>
+      <div className="offers-left">
+
+      </div>
+      <div className="offers-right">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Offers
