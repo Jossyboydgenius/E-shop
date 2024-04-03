b6c9f1c1a03efca6f1dacdacd3242c1adb012c06
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-03 02:58:52 +0000

    E-commerce

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,15 +3,17 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrums/Breadcrum'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
 
 const Product = () => {
-  const {all_product}= useContext(ShopContext)
-  const {productId}= useParams();
-  const product = all_product.find((e)=> e.id === Number(productId));
+  const { all_product } = useContext(ShopContext)
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
   return (
     <div>
-      <Breadcrum product={product}/>
-      <ProductDisplay product={product}/>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
     </div>
   )
 }
