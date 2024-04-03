04304fb7fbb66ee847c19485ba5dcfbb75debf9f
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-03 14:25:53 +0000

    E-commerce

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -16,14 +16,37 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
     const addToCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
         console.log(cartItems);
     }
 
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
-    const contextValue = { all_product, cartItems, addToCart, removeFromCart };
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item))
+                totalAmount += cartItems[item] * itemInfo.new_price;
+            }
+        }
+        return totalAmount;
+    }
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for (const item in cartItems) 
+        {
+            if (cartItems[item] > 0) 
+            {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = { getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
 
     return (
         <ShopContext.Provider value={contextValue}>
