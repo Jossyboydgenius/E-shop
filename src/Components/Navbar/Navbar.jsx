13c5bbb038d5775954d49b863d83e4e63eca7f6a
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-05 05:09:21 +0000

    E-commerce

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,13 +8,13 @@ import { ShopContext } from '../../Context/ShopContext'
 
 const Navbar = () => {
 
-    const [menu,setMenu] = useState("shop");
-    const { getTotalCartItems } = useContext(ShopContext);
-    const menuRef = useRef();
-    const dropdown_toggle = (e) => {
-      menuRef.current.classList.toggle('nav-menu-visible');
-      e.target.classList.toggle('open');
-    }
+  const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className='navbar'>
@@ -24,14 +24,14 @@ const Navbar = () => {
       </div>
       <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to ='/'>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}}  to ='/mens'>Men</Link> {menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womans")}}><Link style={{textDecoration:'none'}}  to ='/womens'>Women</Link> {menu==="womans"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:'none'}}  to ='/kids'>Kids</Link> {menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link> {menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu("mens") }}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link> {menu === "mens" ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu("womans") }}><Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link> {menu === "womans" ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu("kids") }}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link> {menu === "kids" ? <hr /> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
-        <Link to ='/login'><button>Login</button></Link>
-        <Link to ='/cart'><img src={cart_icon} alt="" /></Link>
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
