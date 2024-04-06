facf36306a97280e207550b6c572e867267e9baa
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-06 12:17:41 +0000

    Backend

---
+++ Backend/index.js
@@ -0,0 +1,8 @@
+const port = 4000;
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const mongoose = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path")
+const cors = require("cors")
