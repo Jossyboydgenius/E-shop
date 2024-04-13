6d030130789bf3417c15e32e62b6eabb9d44fe77
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-13 17:57:58 +0000

    Admin

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
+import Admin from './Pages/Admin/Admin'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <Admin/>
     </div>
   )
 }
