ce2b214a3eedb065a5ab5f01b6101a6c8e01eec5
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-13 18:23:22 +0000

    Admin

--- Admin/src/Pages/Admin/Admin.jsx
+++ Admin/src/Pages/Admin/Admin.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Admin.css'
 import SideBar from '../../Components/SideBar/SideBar'
+import { Routes, Route } from 'react-router-dom'
 
 const Admin = () => {
   return (
     <div className='admin'>
       <SideBar/>
+      <Routes>
+        <Route path='/addproduct' element={<AddProduct/>}/>
+        <Route path='/listproduct' element={<ListProduct/>}/>
+      </Routes>
     </div>
   )
 }
