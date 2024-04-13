6b5c76938834d8c185adddf05115a253d7b518f6
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-13 18:11:39 +0000

    Admin

--- Admin/src/Components/SideBar/SideBar.jsx
+++ Admin/src/Components/SideBar/SideBar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './SideBar.css'
 import { Link } from 'react-router-dom'
 import add_product_icon from '../../assets/Product_Cart.svg'
+import list_product_icon from '../../assets/Product_list_icon.svg'
 
 const SideBar = () => {
     return (
@@ -9,7 +10,13 @@ const SideBar = () => {
             <Link to={'/addproduct'} style={{ textDecoration: "none" }}>
                 <div className="sider-item">
                     <img src={add_product_icon} alt="" />
-                    <p>Add Prodcut</p>
+                    <p>Add Product</p>
+                </div>
+            </Link>
+            <Link to={'/listproduct'} style={{ textDecoration: "none" }}>
+                <div className="sidebar-item">
+                    <img src={list_product_icon} alt="" />
+                    <p>Product List</p>
                 </div>
             </Link>
         </div>
