127694828a8fcd89c5876b06ab9479748189ff38
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-16 03:17:44 +0000

    Admin

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar'
 import Admin from './Pages/Admin/Admin';
-import AddProduct from './Components/AddProduct/AddProduct';
-import ListProduct from './Components/ListProduct/ListProduct';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Admin/>
-      <AddProduct/>
-      <ListProduct/>
     </div>
   );
 };
