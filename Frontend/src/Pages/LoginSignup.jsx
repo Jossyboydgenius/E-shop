d0f6d460c7a74a2d6872fa021d236bfbe5eece0b
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-16 20:51:00 +0000

    Frontend

--- Frontend/src/Pages/LoginSignup.jsx
+++ Frontend/src/Pages/LoginSignup.jsx
@@ -16,6 +16,25 @@ const LoginSignup = () => {
 
   const login = async () => {
     console.log("Login Function Executed", formData);
+    let responseData;
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData)
+    })
+      .then((response) => response.json())
+      .then((data) => responseData = data)
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }
+    else {
+      alert(responseData.errors);
+    }
   }
   const signup = async () => {
     console.log("Signup Function Executed", formData);
