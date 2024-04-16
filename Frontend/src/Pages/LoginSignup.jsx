39abda4c69e97b8276107ba8f1e537007e36c1c4
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-16 17:35:37 +0000

    Frontend

--- Frontend/src/Pages/LoginSignup.jsx
+++ Frontend/src/Pages/LoginSignup.jsx
@@ -15,10 +15,10 @@ const LoginSignup = () => {
   }
 
   const login = async () => {
-    console.log("Login Function Executed", formData)
+    console.log("Login Function Executed", formData);
   }
   const signup = async () => {
-    console.log("Signup Function Executed", formData)
+    console.log("Signup Function Executed", formData);
   }
 
   return (
@@ -27,8 +27,8 @@ const LoginSignup = () => {
         <h1>{state}</h1>
         <div className="loginsignup-fields">
           {state === "Sign Up" ? <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' /> : <></>}
-          <input name='email' value={formData.email} onChange={changeHandler}  type="email" placeholder='Email Address' />
-          <input name='password' value={formData.password} onChange={changeHandler}  type="password" placeholder='Password' />
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address' />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
         </div>
         <button onClick={() => { state === "Login" ? login() : signup() }}>Continue</button>
         {state === "Sign Up" ? <p className="loginsignup-login">Already have an account? <span onClick={() => { setState("Login") }}>Login here</span></p>
