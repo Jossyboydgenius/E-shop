fde3c84415ee756fab72c0571c2a3366861ec940
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-17 02:48:48 +0000

    Frontend

--- Frontend/src/Components/NewCollections/NewCollections.jsx
+++ Frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './NewCollections.css'
-import new_collection from '../Assets/new_collections'
 import Item from '../Item/Item'
 
 const NewCollections = () => {
+
+  const [new_collection, setNew_collection] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/newcollections')
+      .then((response) => response.json())
+      .then((data) => setNew_collection(data));
+  }, [])
+
   return (
     <div className='new-collections'>
       <h1>NEW COLLECTIONS</h1>
       <hr />
-    <div className='collections'>
-        {new_collection.map((item,i)=>{
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+      <div className='collections'>
+        {new_collection.map((item, i) => {
+          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
         })}
-    </div>
+      </div>
     </div>
   )
 }
